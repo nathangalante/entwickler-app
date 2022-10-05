f1d5b9fac3f35ffd982ae53247c7ec75cc90ba19
Nathan Galante <galante.nathan@gmail.com>
2022-10-05 16:55:48 +0200

    db queries fertig, database connection funktionert noch nicht, jetzt muss ich an meinem server arbeiten um die queries zu benutzen

--- db.ts
+++ db.ts
@@ -1,13 +1,44 @@
-const db = spicedPg(
+const pg = require("pg");
+const db = pg(
     process.env.DATABASE_URL ||
-        `postgres:postgres:postgres@localhost:5432/petition`
+        `postgres:postgres:postgres@localhost:5432/entwicklerapp`
 );
 
 exports.insertDeveloper = (name: string, email: string, level: boolean) => {
     return db.query(
-        `INSERT INTO users (first, last, email, password)
+        `INSERT INTO users (name, email, level)
     VALUES ($1, $2, $3, $4)
     RETURNING id`,
         [name, email, level]
     );
+};
+
+exports.updateDeveloper = (
+    name: string,
+    email: string,
+    level: boolean,
+    id: number
+) => {
+    return db.query(
+        `UPDATE entwickler SET name = $1, email = $2, level = $3 WHERE id = $4`,
+        [name, email, level, id]
+    );
+};
+
+exports.readDeveloper = (name: string, email: string, level: boolean) => {
+    return db.query(`SELECT * FROM entwickler`, [name, email, level]);
+};
+
+exports.deleteDeveloper = (
+    name: string,
+    email: string,
+    level: boolean,
+    id: number
+) => {
+    return db.query(`DELETE FROM entwickler WHERE id=$1`, [
+        name,
+        email,
+        level,
+        id,
+    ]);
 };
